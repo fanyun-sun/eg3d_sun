1154f2a05ac8718846ed954f97f27462795beec1
Fan-Yun Sun <ssunfanyun@gmail.com>
2022-07-26 08:10:33 -0700

    use 8 gpus

--- eg3d/go.sh
+++ eg3d/go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -ex
 python train.py --outdir=/results/ --cfg=rmtv_google_shoes --data=/workspace \
-	--gpus=16 --batch=32 --gamma=5 --data_image_resolution=256 --use_alpha_background=True --metrics=None
+	--gpus=8 --batch=32 --gamma=5 --data_image_resolution=256 --use_alpha_background=True --metrics=None
